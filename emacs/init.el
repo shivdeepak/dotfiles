58b69140c7cf9574285b36fd711564f1ddcb6006
Shiv Deepak <shivdeepak@gmail.com>
2024-10-09 22:20:58 -0700

    feat: create get-abs-path function

--- emacs/init.el
+++ emacs/init.el
@@ -1,10 +1,13 @@
+(defun get-abs-path (file-name)
+  (let* ((current-file (or load-file-name buffer-file-name))
+         (current-path (file-name-directory current-file)))
+    (expand-file-name file-name current-path)))
+
 (defun load-sub-module (module-name)
   "Load a submodule in the emacs config"
-  (let*
-      ((current-file (or load-file-name buffer-file-name))
-    (current-path (file-name-directory current-file)))
-  (load-file (expand-file-name module-name current-path))))
+  (load-file (get-abs-path module-name)))
 
 (load-sub-module "defaults.el")
 (load-sub-module "editing.el")
 (load-sub-module "git.el")
+(load-sub-module "python.el")
